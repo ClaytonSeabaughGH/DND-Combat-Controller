ea9c8533039ea156abc313da538ae32b2c80e426
ClaytonSeabaughGH <claytonseabaugh@gmail.com>
2025-04-17 16:16:52 -0500

    Create const for player db in app.js file

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-// Import Modules //
+// Import Modules 
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
@@ -7,4 +7,7 @@ const port = 3000;
 app.use(express.json()); // Parse JSON request bodies
 app.use(express.static('public')); // Serve static files (HTML, JS, CSS)
 
-//
+// Initialize Player SQLite database 
+const db = new sqlite3.Database('players.db');
+
+
