21020fa2c612a55d9d25136afe0248e2f31f13db
ClaytonSeabaughGH <claytonseabaugh@gmail.com>
2025-04-17 17:11:36 -0500

    Debugging app path

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // Import Modules 
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 const app = express();
 const port = 3000; 
 
 app.use(express.json()); // Parse JSON request bodies
-app.use(express.static('public')); // Serve static files (HTML, JS, CSS)
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files (HTML, JS, CSS)
 
 // Initialize Player SQLite database 
 const db = new sqlite3.Database('players.db');
